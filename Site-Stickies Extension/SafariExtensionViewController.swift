475f7b27c51d13b95c44851117ba7a8a93fa9a95
George Botros <georgebotros02@gmail.com>
2021-02-05 00:03:07 -0500

    Initial Commit

---
+++ Site-Stickies Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  Site-Stickies Extension
+//
+//  Created by George Botros on 2/5/21.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
