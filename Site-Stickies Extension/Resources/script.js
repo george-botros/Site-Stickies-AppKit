052b3cdb74ce331eea5f9324e38f35c83cbe1709
George Botros <georgebotros02@gmail.com>
2021-02-05 16:58:26 -0500

    Modified script and CSS for note display. Working on bottom padding on note scrolling.

--- Site-Stickies Extension/Resources/script.js
+++ Site-Stickies Extension/Resources/script.js
@@ -4,23 +4,11 @@ if(window.top === window) {
     function createNewNote(event) {
                 if (event.name == "toolbarItemClicked") {
                     console.log("creating a new note 📝", event)
-                    var stickycontainer = document.createElement("div");
-                    stickycontainer.setAttribute("class", "sticky-container")
-                    document.body.appendChild(stickycontainer);
-                    
-                        var stickyouter = document.createElement("div");
-                        stickyouter.setAttribute("class", "sticky-outer")
-                        stickycontainer.appendChild(stickyouter)
-
-                            var sticky = document.createElement("div")
-                            sticky.setAttribute("class", "sticky")
-                            stickyouter.appendChild(sticky)
-                    
-                                var stickycontent = document.createElement("div")
-                                stickycontent.setAttribute("class", "sticky-content")
-                                stickycontent.setAttribute("contenteditable", "true")
-                                stickycontent.innerHTML = "New note 📝"
-                                sticky.appendChild(stickycontent)
-                }
+                    var sticky = document.createElement("div");
+                    sticky.setAttribute("class", "sticky")
+                    sticky.setAttribute("contenteditable", "true")
+                    document.body.appendChild(sticky);
+                    sticky.innerHTML = "New note 📝"
             }
+    }
 }
